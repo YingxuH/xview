b1736cc4a38a776042b50842821319f1c2e333bf
YingxuH <yingxu.he1998@gmail.com>
2023-04-26 09:20:54 +0800

    make the caller run

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,16 @@
+FREE_GPT_URL = "https://gptai.cloud"
+
 INPUT_PROMPT = "Act as an image captioner to provide an accurate caption in 2-5 sentences for a 256 x 256 satellite image based on a set of object annotations. I want you to describe only the sizes and positional relationships of these objects in plain English. I want you to describe features using English instead of throwing the exact numbers overwhelmingly. Here is the annotation"
 
 DO_NOT_THROW_NUMBER_PROMPT = "Please describe in English rather than throwing out numbers overwhelmingly. Try one more time."
 DO_NOT_MAKE_UP_PROMPT = "I want you to only describe the content of the annotation. Do not make anything up. Try one more time."
 DO_NOT_MAKE_UP_KEYWORDS = ["colored", "colour", "color", "blue", "red", "yellow", "orange", "brown", "color", "window", "windows", "roof", "flat", "fence", "dark", "darker"]
 DO_NOT_MENTION_ANNOTATION_KEYWORDS = ["annotation", "annotations", "marked", "mark", "marks"]
+FOLLOW_TASK_FORMAT_PROMPT = "Please make sure the answer task 3 starts with 'Task 3'. Please try again."
+FOLLOW_CAPTION_FORMAT_PROMPT = "Please start each caption with 'CAP'. Please try one more time."
+FOLLOW_CAPTION_LENGTH_PROMPT = "Please make sure each caption has no longer than 32 tokens. Please try one more time."
 
-
-PROMPT_PREFIX = ["Assume you are Joshua, an experienced data analyst. I will give you a detailed description of objects in an image and will ask you to complete a few tasks:"]
+PROMPT_PREFIX = ["Assume you are Joshua, an experienced data analyst. I will give you a detailed description of objects in an image and will ask you to complete a few tasks"]
 
 PROMPT_SUFFIX = [
     ["Task 1: Determine spatial relationships between objects, in terms of directions, distances, etc.", 
@@ -14,5 +18,5 @@ PROMPT_SUFFIX = [
      "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with 'CAP', with no longer than 32 tokens."],
     ["Task 1: Determine spatial relations between objects.",
      "Task 2: Extract high-level spatial patterns between the objects, such as direction, clustering, dispersion, encirclement, interposition, etc.",
-     "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with "CAP", with no longer than 32 tokens."]
+     "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with 'CAP', with no longer than 32 tokens."]
 ]
--- src/gpt_caller.py
+++ src/gpt_caller.py
@@ -1,10 +1,12 @@
 import re
+import os
 import time
+import json
+from pathlib import Path
 import concurrent.futures as futures
 
 import openai
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from gensim.utils import tokenize
 
@@ -12,53 +14,81 @@ from src import constants
 
 
 class FreeChatGptCaller:
-    def __init__(self, input_prompt, save=True):
-        self.input_prompt = input_prompt 
+    def __init__(self, save=True):
+        self.prefix_prompt = constants.PROMPT_PREFIX[0]
+        self.suffix_prompt = ", ".join(constants.PROMPT_SUFFIX[0])
+        self.caption_task_regex = "Task\s*3[:=-](.*)$"
+        self.caption_head_regex = "CAP\s*\d*\s*[:=-]"
         self.save = save
-        
+
         self.dummy_response = ''
         self.save_directory = "log/free/"
-        self.time_limit = 60
+        self.time_limit = 120
         self.do_not_throw_number_prompt = constants.DO_NOT_THROW_NUMBER_PROMPT
         self.do_not_make_up_prompt = constants.DO_NOT_MAKE_UP_PROMPT
         self.do_not_make_up_keywords = constants.DO_NOT_MAKE_UP_KEYWORDS
-        
+        self.follow_task_format_prompt = constants.FOLLOW_TASK_FORMAT_PROMPT
+        self.follow_caption_format_prompt = constants.FOLLOW_CAPTION_FORMAT_PROMPT
+        self.follow_caption_length_prompt = constants.FOLLOW_CAPTION_LENGTH_PROMPT
+
         self.history = []
         self.session = []
-        
-        self.target_url = "https://gptai.cloud"
+
+        self.target_url = constants.FREE_GPT_URL
+        self.driver = None
+        self.init_webdriver()
+
+    def start_session(self):
+        self.session = []
+
+    def clear_session(self):
+        self.history.append(self.session)
+        self.session = []
+        clear_button = self.get_buttons()[0]
+        clear_button.click()
+
+    def clear_history(self):
+        if self.save:
+            Path(self.save_directory).mkdir(parents=True, exist_ok=True)
+            file_name = f"{time.strftime('%y%m%d%H%M%S')}.json"
+            with open(os.path.join(self.save_directory, file_name), "w") as f:
+                json.dump(self.history, f, indent=4)
+
+        self.history = []
+
+    def init_webdriver(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.target_url)
-        
+
+    def quit_webdriver(self):
+        self.driver.quit()
+        self.driver = None
+
     def get_text_area(self):
         return self.driver.find_element(By.XPATH, "//textarea")
-        
+
     def get_buttons(self):
         buttons = self.driver.find_elements(By.XPATH, f"//button")
+        clear_button, send_button = None, None
         for button in buttons:
             if button.get_attribute("title") == "Clear":
                 clear_button = button
             else:
                 send_button = button
         return clear_button, send_button
-    
+
+    def has_xpath_element(self, path):
+        elements = self.driver.find_elements(By.XPATH, path)
+        return len(elements) > 0
+
     def _get_response(self):
-        prev_txt = None
-        txt = ""
-        equal_count = 0
-        
-        while (prev_txt is None) or (prev_txt != txt) or equal_count <= 5:
+        while not self.has_xpath_element("//textarea"):
             time.sleep(1)
-            responses = self.driver.find_elements(By.XPATH, "//div[@class='flex gap-3 rounded-lg']")            
-            if not responses:
-                continue
-            if txt == responses[-1].text:
-                equal_count += 1
-            prev_txt = txt
-            txt = responses[-1].text
-            
-        return txt
-    
+
+        responses = self.driver.find_elements(By.XPATH, "//div[@class='flex gap-3 rounded-lg']")
+
+        return responses[-1].text
+
     def get_response(self):
         # Main
         with futures.ThreadPoolExecutor(max_workers=1) as executor:
@@ -67,70 +97,68 @@ class FreeChatGptCaller:
             executor._threads.clear()
             futures.thread._threads_queues.clear()
         return resp
-        
-    def start_session(self):
-        self.session = []
-        
-    def clear_session(self):
-        self.history.append(self.session)
-        self.session = []
-        clear_button = self.get_buttons()[0]
-        clear_button.click()
-        
-    def clear_history(self):
-        if self.save:
-            Path(self.save_directory).mkdir(parents=True, exist_ok=True)
-            file_name = f"{time.strftime('%y%m%d%H%M%S')}.json"
-            with open(os.path.join(self.save_directory, file_name), "w") as f:
-                json.dump(self.history, f, indent=4)
-                
-        self.history = []
-        
-    
-    def call(self, input_message, retry=3):
-        if retry == 0:
-            return self.dummy_response
-        
-        try:
-            input_ele = self.get_text_area()
-            input_ele.clear()
-            input_ele.send_keys(input_message)
-            send_button = self.get_buttons()[1]
-            send_button.click()
-            response = self.get_response()
-        except Exception as e:
-            print(f"retry {retry}: {e}")
-            return self.call(input_message, retry=retry-1)
-                        
+
+    def call(self, input_message):
+        input_ele = self.get_text_area()
+        input_ele.clear()
+        input_ele.send_keys(input_message)
+        _, send_button = self.get_buttons()
+        send_button.click()
+        response = self.get_response()
+
         self.session.append({"role": "user", "content": input_message})
         self.session.append({"role": "assistant", "content": response})
         return response
-    
+
     def do_not_throw_number_validation(self, polygons, response):
         image_size_mentions = 2
         other_mentions = min(len(polygons), 2)
-        
+
         return len(re.findall("\d+", response)) <= (image_size_mentions + other_mentions)
-    
+
     def do_not_make_up_validation(self, polygons, response):
         counts = [token for token in tokenize(response) if token in self.do_not_make_up_keywords]
-        
+
         return len(counts) < 2
-        
-    def query_image(self, polygons, retry=3):
-        self.start_session()
-        input_message = f"{self.input_prompt}: {polygons}".replace("\n", "").replace("\'", "")
+
+    def caption_validation(self, response):
+        task_group = re.search(self.caption_task_regex, response.replace("\n", ""), re.IGNORECASE)
+        if task_group is None:
+            return self.follow_task_format_prompt
+
+        captions_heads = re.findall(self.caption_head_regex, task_group.group(1))
+        if len(captions_heads) == 0:
+            return self.follow_caption_format_prompt
+
+        captions = re.split(self.caption_head_regex, task_group.group(1))[1:]
+        captions_lens = [len(list(tokenize(cap.strip()))) > 25 for cap in captions]
+        if any(captions_lens):
+            return self.follow_caption_length_prompt
+
+        return ""
+
+    def _query_image(self, polygons):
+        input_message = f"{self.prefix_prompt}: Description: '{polygons}' Tasks: {self.suffix_prompt}"
 
         response = self.call(input_message)
-        
-        if not self.do_not_throw_number_validation(polygons, response):
-            print(f"follow up: {response}")
-            response = self.call(self.do_not_throw_number_prompt)
-        
-        if not self.do_not_make_up_validation(polygons, response):
-            print(f"follow up: {response}")
-            response = self.call(self.do_not_make_up_prompt)
-            
+
+        follow_up_prompt = self.caption_validation(response)
+        if follow_up_prompt != "":
+            response = self.call(follow_up_prompt)
+
+        return response
+
+    def query_image(self, polygons, retry=3):
+        if retry == 0:
+            return self.dummy_response
+
+        self.start_session()
+        try:
+            response = self._query_image(polygons)
+        except Exception as e:
+            print(f"retry {retry}", e)
+            response = self.query_image(polygons, retry=retry-1)
+
         self.clear_session()
         return response
     
