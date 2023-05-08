a97b7437515c3c1d39614c3c7f46190440d6ae7a
YingxuH <yingxu.he1998@gmail.com>
2023-05-08 09:08:46 +0800

    update multithreading

--- src/constants.py
+++ src/constants.py
@@ -10,9 +10,11 @@ DO_NOT_MAKE_UP_KEYWORDS = ["colored", "colour", "color", "blue", "red", "yellow"
 DO_NOT_MENTION_ANNOTATION_KEYWORDS = ["annotation", "annotations", "marked", "mark", "marks"]
 FOLLOW_TASK_FORMAT_PROMPT = "Please make sure the answer task 3 starts with 'Task 3'. Please try again."
 FOLLOW_CAPTION_FORMAT_PROMPT = "Please start each caption with 'CAP'. Please try one more time."
-FOLLOW_CAPTION_LENGTH_PROMPT = "Please make sure each caption has no longer than 32 tokens. Please try one more time."
+FOLLOW_CAPTION_LENGTH_PROMPT = "Please shorten the captions with each no longer than 32 tokens while keeping the original meaning. Let's do it!"
 
-PROMPT_PREFIX = ["Assume you are Joshua, an experienced data analyst. I will give you a detailed description of objects in an image and will ask you to complete a few tasks"]
+PROMPT_PREFIX = [
+    "Assume you are Joshua, an experienced data analyst. I will give you a detailed description of objects in an image and will ask you to complete a few tasks"
+]
 
 PROMPT_SUFFIX = [
     ["Task 1: Determine spatial relationships between objects, in terms of directions, distances, etc.", 
@@ -20,5 +22,30 @@ PROMPT_SUFFIX = [
      "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with 'CAP', with no longer than 32 tokens."],
     ["Task 1: Determine spatial relations between objects.",
      "Task 2: Extract high-level spatial patterns between the objects, such as direction, clustering, dispersion, encirclement, interposition, etc.",
-     "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with 'CAP', with no longer than 32 tokens."]
+     "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with 'CAP', with no longer than 32 tokens."],
+    ["Task 1: Analyze the input description to identify the objects present in the image and their spatial coordinates and sizes.",
+     "Task 2: Determine the spatial relationships between the objects, such as their relative positions, directions, clustering, dispersion, encirclement, interposition, etc.",
+     "Task 3: Use the identified spatial relationships to generate one or more independent image captions that accurately and vividly describe the high-level significant spatial patterns present in the image. Each caption should start with ‘CAP’ and be no longer than 32 tokens."],
+    ["Task 1: Determine spatial relations between objects.",
+     "Task 2: Extract high-level spatial patterns between the objects, such as adjacency (objects very close to other objects), clustering (objects concentrated in the same area), formation (objects form a line/arc/circle shape, etc.), dispersion (objects scattered around), encirclement (objects surrounded by others), interposition (objects between others), etc.",
+     "Task 3: Generate one or more independent image captions with high-level significant spatial patterns. Each starts with 'CAP', with no longer than 32 tokens."],
+    ["Task 1: Identify and extract the coordinates, size, and type of each object in the input description.",
+     "Task 2: Determine the spatial relationships between objects, such as their relative positions (e.g., top left, bottom right), distances, and formations (e.g., line, arc, circle)."
+     "Task 3: Generate one or more independent image captions that accurately describe the significant spatial patterns and relationships between the objects. Each caption should start with ‘Caption’ and be no longer than 32 tokens."],
+    ["Task 1: Identify the objects in the image and their locations and sizes. Use the format: ‘There is a/an [object] at [x,y] with a size of [pixels].’ Separate each object with a period.",
+     "Task 2: Compare the locations and sizes of the objects and describe how they relate to each other in terms of distance and direction. Use the format: ‘[Object A] is [relation] to [object B].’ Separate each relation with a period.",
+     "Task 3: Summarize the main spatial patterns of the objects in one or more sentences. Start each sentence with ‘CAP’. Use descriptive words and phrases to make the captions vivid and accurate."],
+    ["Task 1: Identify the objects in the image and their locations and sizes.",
+     "Task 2: Determine the spatial relationships between the objects, such as their relative positions and distances from each other.",
+     "Task 3: Identify any patterns or formations in the arrangement of the objects, such as lines, arcs, clusters, or dispersion.",
+     "Task 4: Generate one or more independent image captions that accurately describe the significant spatial patterns and relationships between the objects. Each caption should start with ‘CAP’ and be no longer than 32 tokens."],
+    ["Task 1: Identify the objects in the image and their locations and sizes. Use the format: ‘There is a/an [object] at [x,y] with a size of [pixels].’ Separate each object with a period.",
+     "Task 2: Compare the locations and sizes of the objects and describe how they relate to each other in terms of distance and direction. Use the format: ‘[Object A] is [relation] to [object B].’ Use words like 'close to', 'on the top', 'on the bottom' , 'on the left', 'on the right', etc. Separate each relation with a period.",
+     "Task 3: Identify any patterns or formations in the arrangement of the objects, such as lines, arcs, clusters, or dispersion.",
+     "Task 4: Generate one or more independent image captions that accurately describe the significant spatial patterns and relationships between the objects. Each caption should start with ‘CAP’ and be no longer than 32 tokens."],
+    ["Task 1: Identify the objects in the image and their locations and sizes. Use the format: ‘There is a/an [object] at [x,y] with a size of [pixels].’ Separate each object with a period.",
+     "Task 2: Compare the locations and sizes of the objects and describe how they relate to each other in terms of distance and direction. Use the format: ‘[Object A] is [relation] to [object B].’ Separate each relation with a period.",
+     "Task 3: Identify any patterns or formations in the arrangement of the objects, such as lines, arcs, clusters, or dispersion.",
+     "Task 4: Generate one or more independent image captions that accurately describe the significant spatial patterns and relationships between the objects. Each caption should start with ‘CAP’ and be no longer than 32 tokens."]
+
 ]
--- src/gpt_caller.py
+++ src/gpt_caller.py
@@ -12,29 +12,11 @@ from gensim.utils import tokenize
 
 from src import constants
 
-def _unified_gpt_create(model, message, *args, **kwargs):
-    if model in constants.TEXT_COMPLETION_MODELS:
-        return openai.Completion.create(
-            model=model,
-            prompt=message,
-            *args,
-            **kwargs
-        )
-    elif model in constants.CHAT_COMPLETION_MODELS:
-        return openai.ChatCompletion.create(
-            model=model,
-            messages=message,
-            *args,
-            **kwargs
-        )
-    else:
-        raise Exception(f"unknown model name: {model}")
-
 
 class FreeChatGptCaller:
-    def __init__(self, save=True):
-        self.prefix_prompt = constants.PROMPT_PREFIX[0]
-        self.suffix_prompt = ", ".join(constants.PROMPT_SUFFIX[0])
+    def __init__(self, prefix_index=0, suffix_index=0, save=True):
+        self.prefix_prompt = constants.PROMPT_PREFIX[prefix_index]
+        self.suffix_prompt = " ".join(constants.PROMPT_SUFFIX[suffix_index])
         self.caption_task_regex = "Task\s*3[:=-](.*)$"
         self.caption_head_regex = "CAP\s*\d*\s*[:=-]"
         self.save = save
@@ -74,9 +56,15 @@ class FreeChatGptCaller:
 
         self.history = []
 
-    def init_webdriver(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get(self.target_url)
+    def init_webdriver(self, retry=3):
+        if retry == 0:
+            return
+
+        try:
+            self.driver = webdriver.Chrome()
+            self.driver.get(self.target_url)
+        except Exception:
+            self.init_webdriver(retry=retry-1)
 
     def quit_webdriver(self):
         self.driver.quit()
@@ -128,17 +116,6 @@ class FreeChatGptCaller:
         self.session.append({"role": "assistant", "content": response})
         return response
 
-    def do_not_throw_number_validation(self, polygons, response):
-        image_size_mentions = 2
-        other_mentions = min(len(polygons), 2)
-
-        return len(re.findall("\d+", response)) <= (image_size_mentions + other_mentions)
-
-    def do_not_make_up_validation(self, polygons, response):
-        counts = [token for token in tokenize(response) if token in self.do_not_make_up_keywords]
-
-        return len(counts) < 2
-
     def caption_validation(self, response):
         task_group = re.search(self.caption_task_regex, response.replace("\n", ""), re.IGNORECASE)
         if task_group is None:
@@ -175,13 +152,107 @@ class FreeChatGptCaller:
             response = self._query_image(polygons)
         except Exception as e:
             print(f"retry {retry}", e)
+            self.quit_webdriver()
+            self.init_webdriver()
             response = self.query_image(polygons, retry=retry-1)
 
         self.clear_session()
         return response
-    
-    
-class ChatGptCaller:
+
+
+class APIGptCaller:
+    def __init__(self, model_name, system_message, limit=2500, save=True):
+        self.model_name = model_name
+        self.system_message = system_message
+
+        self.total_use = 0
+        self.limit = limit * 1000
+        self.dummy_response = ''
+        self.save = save
+        self.save_directory = "log/"
+
+        self.history = []
+        self.session = []
+
+    def start_session(self):
+        self.session = [{"role": "system", "content": self.system_message}]
+
+    def clear_session(self):
+        self.history.append(self.session)
+        self.session = []
+
+    def clear_history(self):
+        if self.save:
+            Path(self.save_directory).mkdir(parents=True, exist_ok=True)
+            file_name = f"{time.strftime('%y%m%d%H%M%S')}.json"
+            with open(os.path.join(self.save_directory, file_name), "w") as f:
+                json.dump(self.history, f, indent=4)
+
+        self.history = []
+
+    def _call(self, input_message):
+        return 0, self.dummy_response
+
+    def call(self, input_message, retry=3):
+        if retry == 0:
+            return self.dummy_response
+
+        try:
+            use, response = self._call(
+                input_message=input_message
+            )
+        except Exception as e:
+            print(f"retry {retry}: {e}")
+            return self.call(input_message, retry=retry-1)
+
+        self.session.append({"role": "user", "content": input_message})
+        self.session.append({"role": "assistant", "content": response})
+        self.total_use += use
+        return response
+
+    def query_image(self, input_message, retry=3):
+        self.start_session()
+        response = self.call(input_message, retry=retry)
+        self.clear_session()
+        return response
+
+
+class ChatGptCaller(APIGptCaller):
+    def __init__(self, model_name, system_message, limit=2500, save=True):
+        super().__init__(model_name, system_message, limit, save)
+
+    def _call(self, input_message):
+        messages = self.session + [{"role": "user", "content": input_message}]
+
+        result = openai.ChatCompletion.create(
+                model=self.model_name,
+                messages=messages,
+                max_tokens=512,
+                temperature=0.7
+            )
+
+        use = result["usage"]["total_tokens"]
+        response = result['choices'][0]['message']['content']
+        return use, response
+
+
+class CompletionGptCaller(APIGptCaller):
+    def __init__(self, model_name, system_message, limit=2500, save=True):
+        super().__init__(model_name, system_message, limit, save)
+
+    def _call(self, input_message):
+        result = openai.Completion.create(
+            model=self.model_name,
+            prompt=input_message,
+            max_tokens=512,
+            temperature=0.7
+        )
+
+        use = result["usage"]["total_tokens"]
+        response = result['choices'][0]["text"]
+        return use, response
+
+class _ChatGptCaller:
     def __init__(self, model_name, system_message, mode="chat", limit=2500, save=True):
         self.model_name = model_name
         self.system_message = system_message
