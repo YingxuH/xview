70ce414b738b769aad28639a454e471fa98a53f2
He Yingxu <yingxu.he1998@gmail.com>
2023-09-05 20:39:25 +0800

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -61,3 +61,28 @@ class ClusterExtractor:
         weights = weights_matrix[np.triu_indices(n=weights_matrix.shape[0], k=1)]
         edges_types = np.array(types)[edges]
         upper_thres = np.array([distance_thres_percentile[t] for t in edges_types[:, 0]])
+
+        valid_edges_condition = edges_types[:, 0] == edges_types[:, 1]
+        none_empty_weights_condition = weights > 0
+        valid_weights_condition = weights < upper_thres
+
+        valid_indices = np.where(valid_weights_condition & none_empty_weights_condition & valid_weights_condition)[0]
+        valid_edges = edges[valid_indices]
+        valid_weights = weights[valid_indices]
+        valid_edges_types = edges_types[valid_indices]
+
+        clusters = get_clusters(x.shape[0], valid_edges)
+
+        invalid_indices = np.where(none_empty_weights_condition & ~(valid_weights_condition & valid_edges_condition))[0]
+        invalid_edges = edges[invalid_indices]
+        connect_array = np.concatenate([clusters[invalid_edges], np.flip(clusters[invalid_edges], axis=1)])
+
+        for i, cid in enumerate(np.unique(clusters)):
+            c_types = [original_types[i] for i, t in enumerate(original_types) if clusters[i] == cid]
+            objects_common_parents = self.tree.find_common_parent(np.unique(c_types))
+            if detect_line_shape(x[clusters == cid], ae_thres=0.7):
+                print(f"group {cid} contains a line of {objects_common_parents}")
+            else:
+                print(f"group {cid} contains some {objects_common_parents}")
+
+        describe_relations(connect_array, x, clusters)
